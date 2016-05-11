7a47a64f320750163d6711ee1160eb78738fea52
serg2801 <serg-koreckij@yandex.ru>
2016-05-11 10:57:25 +0000

    update redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,8 @@
 ShopifyApp.configure do |config|
   config.api_key = "e520db04e2eff2f5052dbb73ff673834"
   config.secret = "25f72f67fe9c83066ea8a8d8a90b2bdf"
-  config.redirect_uri = "<redirect_uri>"
+  # config.redirect_uri = "<redirect_uri>"
+  config.redirect_uri = "https://pricelocation-serg2801.c9users.io/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
