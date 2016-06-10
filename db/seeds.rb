bc2058bf0cc382a3a6c18af9ab072489bda50fd4
Mykyta Troianov <nicktroyanov@gmail.com>
2016-06-10 16:17:06 +0300

    added seeds to create regions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Region.create([{ name: "UK" }, { name: "Ireland" }, { name: "Rest of Europe" }, { name: "Rest of World" }])
